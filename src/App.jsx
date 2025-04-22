65ed80992dfc4614490ef0201e9a84a3142904ad
ratheevivek123 <ratheevivek521@gmail.com>
2025-04-23 01:49:12 +0530

    intro-to-react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-300">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello Navbar</h1>
+      <h1>Hello Navbar</h1>;
+    </div>
+  );
+}
+
+export default Navbar;
